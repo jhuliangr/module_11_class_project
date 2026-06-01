e952475ad05e819aaa5ae14844ed16449d77b4df
jhuliangr <jhpiano9731@gmail.com>
2026-06-01 21:49:27 +0200

    Add testing

--- knip.config.ts
+++ knip.config.ts
@@ -2,8 +2,10 @@ import { type KnipConfiguration } from "knip";
 
 const config: KnipConfiguration = {
   $schema: "https://unpkg.com/knip@6/schema.json",
-  ignoreFiles: ["dist/**", "web-build/**", "babel.config.cjs"],
-  ignoreDependencies: ["expo-updates", "expo-system-ui"],
+  ignoreDependencies: ["expo-asset", "expo-modules-core", "expo-updates"],
+  ignoreIssues: {
+    "src/shared/**": ["exports", "types"],
+  },
 };
 
 export default config;
